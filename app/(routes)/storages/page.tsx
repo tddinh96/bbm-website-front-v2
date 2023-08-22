ac43692054296f99ae4fcd6e32b0fe9bbe1bad52
tddinh96 <78867116+tddinh96@users.noreply.github.com>
2023-08-21 20:00:35 -0700

    Add content to storage page

--- app/(routes)/storages/page.tsx
+++ app/(routes)/storages/page.tsx
@@ -59,9 +59,146 @@ const Storages = () => {
     if(!isMounted){
         return null;
     }
-    return ( 
-        <div>Storage Pages v2 - WIP- This is my starting page</div>
-     );
+    return (
+        <div className="bg-white">
+          <div className="pt-6">
+            <nav aria-label="Breadcrumb">
+              <ol role="list" className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                <li className="text-sm">
+                  <a href={product.href} aria-current="page" className="font-medium text-gray-500 hover:text-gray-600">
+                    {product.name}
+                  </a>
+                </li>
+              </ol>
+            </nav>
+    
+            {/* Image gallery */}
+            <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+              <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
+                <img
+                  src={product.images[0].src}
+                  alt={product.images[0].alt}
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+              <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+                <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+                  <img
+                    src={product.images[1].src}
+                    alt={product.images[1].alt}
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+                <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+                  <img
+                    src={product.images[2].src}
+                    alt={product.images[2].alt}
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+              </div>
+              <div className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
+                <img
+                  src={product.images[3].src}
+                  alt={product.images[3].alt}
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+            </div>
+    
+            {/* Product info */}
+            <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+              <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{product.name}</h1>
+              </div>
+    
+              {/* Slip */}
+              <div className="mt-4 lg:row-span-3 lg:mt-0">
+                <h2 className="sr-only">Product information</h2>
+                <p className="text-3xl tracking-tight text-gray-900">{product.slip}</p>
+    
+                <form className="mt-10">
+                  {/* Colors */}
+                  <div>
+                    <h3 className="text-sm font-medium text-gray-900">Color</h3>
+    
+                    <RadioGroup value={selectedColor} onChange={setSelectedColor} className="mt-4">
+                      <RadioGroup.Label className="sr-only">Choose a color</RadioGroup.Label>
+                      <div className="flex items-center space-x-3">
+                        {product.colors.map((color) => (
+                          <RadioGroup.Option
+                            key={color.name}
+                            value={color}
+                            className={({ active, checked }) =>
+                              classNames(
+                                color.selectedClass,
+                                active && checked ? 'ring ring-offset-1' : '',
+                                !active && checked ? 'ring-2' : '',
+                                'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none'
+                              )
+                            }
+                          >
+                            <RadioGroup.Label as="span" className="sr-only">
+                              {color.name}
+                            </RadioGroup.Label>
+                            <span
+                              aria-hidden="true"
+                              className={classNames(
+                                color.class,
+                                'h-8 w-8 rounded-full border border-black border-opacity-10'
+                              )}
+                            />
+                          </RadioGroup.Option>
+                        ))}
+                      </div>
+                    </RadioGroup>
+                  </div>
+    
+                </form>
+                {/* Dry Storage */}
+                <div className="mt-6">
+                <p className="text-3xl tracking-tight text-gray-900">Dry Storage</p>
+
+                </div>
+
+              </div>
+    
+              <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+                {/* Description and details */}
+                <div>
+                  <h3 className="sr-only">Description</h3>
+    
+                  <div className="space-y-6">
+                    <p className="text-base text-gray-900">{product.description}</p>
+                  </div>
+                </div>
+    
+                <div className="mt-10">
+                  <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
+    
+                  <div className="mt-4">
+                    <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
+                      {product.highlights.map((highlight) => (
+                        <li key={highlight} className="text-gray-400">
+                          <span className="text-gray-600">{highlight}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+    
+                <div className="mt-10">
+                  <h2 className="text-sm font-medium text-gray-900">Details</h2>
+    
+                  <div className="mt-4 space-y-6">
+                    <p className="text-sm text-gray-600">{product.details}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
 }
  
 export default Storages;
