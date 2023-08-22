04508c3c7908314b5591d6a6311224cb8016faf1
tddinh96 <78867116+tddinh96@users.noreply.github.com>
2023-08-21 20:08:23 -0700

    reverse changes of storage page

--- app/(routes)/storages/page.tsx
+++ app/(routes)/storages/page.tsx
@@ -1,8 +1,76 @@
+// "use client";
+// import { useEffect, useState } from "react";
+
+// const Storages = () => {
+//     const [isMounted, setIsMounted] = useState(false);
+
+//     useEffect(()=> {
+//         setIsMounted(true);
+//     }, []);
+
+//     if(!isMounted){
+//         return null;
+//     }
+//     return ( 
+//         <div>Storage Pages v2 - WIP- This is my starting page</div>
+//      );
+// }
+ 
+// export default Storages;
+
 "use client";
 import { useEffect, useState } from "react";
+import { RadioGroup } from '@headlessui/react';
+
+
+const product = {
+    name: 'Boat Storage',
+    slip: 'Uncovered / Covered Slip',
+    dryStorage: '',
+    href: '#',
+    images: [
+      {
+        src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
+        alt: 'Two each of gray, white, and black shirts laying flat.',
+      },
+      {
+        src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg',
+        alt: 'Model wearing plain black basic tee.',
+      },
+      {
+        src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg',
+        alt: 'Model wearing plain gray basic tee.',
+      },
+      {
+        src: 'https://static.wixstatic.com/media/dbaa91_b06ea863c6df478399e5fca41a4b116d~mv2.png/v1/fill/w_537,h_666,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Slip%20Storage%20FINAL%20(1).png',
+        alt: 'Model wearing plain white basic tee.',
+      },
+    ],
+    colors: [
+      { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+      { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
+      { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+    ],
+    description:
+      'Find your sanctuary at Big Break Marina, where we offer fortified dry storage for trailers and sheltered berths for boats. Our mission is to shield your valuable investment from the elements, providing not just a storage facility, but a bastion for your prized vessels. With seamless access that ensures your next voyage is only ever a heartbeat away, we make it easy for you to sail whenever the mood strikes. Place your trust in our robust facilities and outstanding service, committed to safeguarding your vessel round the clock. At Big Break Marina, your peace of mind is our greatest reward.',
+    highlights: [
+      'Hand cut and sewn locally',
+      'Dyed with our proprietary colors',
+      'Pre-washed & pre-shrunk',
+      'Ultra-soft 100% cotton',
+    ],
+    details:
+      'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+  }
+  
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+  }
+
 
 const Storages = () => {
     const [isMounted, setIsMounted] = useState(false);
+    const [selectedColor, setSelectedColor] = useState(product.colors[0])
 
     useEffect(()=> {
         setIsMounted(true);
@@ -11,9 +79,150 @@ const Storages = () => {
     if(!isMounted){
         return null;
     }
-    return ( 
-        <div>Storage Pages v2 - WIP- This is my starting page</div>
-     );
+    return (
+        <div className="bg-white">
+          <div className="pt-6">
+            <nav aria-label="Breadcrumb">
+              <ol role="list" className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                <li className="text-sm">
+                  <a href={product.href} aria-current="page" className="font-medium text-gray-500 hover:text-gray-600">
+                    {product.name}
+                  </a>
+                </li>
+              </ol>
+            </nav>
+    
+            {/* Image gallery */}
+            <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+              <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
+                <img
+                  src={product.images[0].src}
+                  alt={product.images[0].alt}
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+              <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+                <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+                  <img
+                    src={product.images[1].src}
+                    alt={product.images[1].alt}
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+                <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+                  <img
+                    src={product.images[2].src}
+                    alt={product.images[2].alt}
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+              </div>
+              <div className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
+                <img
+                  src={product.images[3].src}
+                  alt={product.images[3].alt}
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+            </div>
+    
+            {/* Product info */}
+            <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+              <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{product.name}</h1>
+              </div>
+    
+              {/* Slip */}
+              <div className="mt-4 lg:row-span-3 lg:mt-0">
+                <h2 className="sr-only">Product information</h2>
+                <p className="text-3xl tracking-tight text-gray-900">{product.slip}</p>
+    
+                <form className="mt-10">
+                  {/* Colors */}
+                  <div>
+                    <h3 className="text-sm font-medium text-gray-900">Color</h3>
+    
+                    <RadioGroup value={selectedColor} onChange={setSelectedColor} className="mt-4">
+                      <RadioGroup.Label className="sr-only">Choose a color</RadioGroup.Label>
+                      <div className="flex items-center space-x-3">
+                        {product.colors.map((color) => (
+                          <RadioGroup.Option
+                            key={color.name}
+                            value={color}
+                            className={({ active, checked }) =>
+                              classNames(
+                                color.selectedClass,
+                                active && checked ? 'ring ring-offset-1' : '',
+                                !active && checked ? 'ring-2' : '',
+                                'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none'
+                              )
+                            }
+                          >
+                            <RadioGroup.Label as="span" className="sr-only">
+                              {color.name}
+                            </RadioGroup.Label>
+                            <span
+                              aria-hidden="true"
+                              className={classNames(
+                                color.class,
+                                'h-8 w-8 rounded-full border border-black border-opacity-10'
+                              )}
+                            />
+                          </RadioGroup.Option>
+                        ))}
+                      </div>
+                    </RadioGroup>
+                  </div>
+    
+                </form>
+                {/* Dry Storage */}
+                <div className="mt-6">
+                <p className="text-3xl tracking-tight text-gray-900">Dry Storage</p>
+
+                </div>
+
+              </div>
+    
+              <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+                {/* Description and details */}
+                <div>
+                  <h3 className="sr-only">Description</h3>
+    
+                  <div className="space-y-6">
+                    <p className="text-base text-gray-900">{product.description}</p>
+                  </div>
+                </div>
+    
+                <div className="mt-10">
+                  <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
+    
+                  <div className="mt-4">
+                    <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
+                      {product.highlights.map((highlight) => (
+                        <li key={highlight} className="text-gray-400">
+                          <span className="text-gray-600">{highlight}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+    
+                <div className="mt-10">
+                  <h2 className="text-sm font-medium text-gray-900">Details</h2>
+    
+                  <div className="mt-4 space-y-6">
+                    <p className="text-sm text-gray-600">{product.details}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
 }
  
 export default Storages;
+
+
+
+  
