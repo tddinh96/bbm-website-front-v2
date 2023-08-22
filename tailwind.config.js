d096a78b195a746e4d587256653235632b6bbdac
tddinh96 <78867116+tddinh96@users.noreply.github.com>
2023-08-21 21:15:58 -0700

    reverse tailwind.config.js file to original

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      gridTemplateRows: {
-        '[auto,auto,1fr]': 'auto auto 1fr',
-      },
+
     },
   },
   plugins: [
     // ...
-    require('@tailwindcss/aspect-ratio'),
+   
   ],
 }
