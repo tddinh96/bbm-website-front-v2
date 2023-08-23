72ba1c0a1e7b67c438b0e804a0280d02b1c8e372
tddinh96 <78867116+tddinh96@users.noreply.github.com>
2023-08-22 18:53:03 -0700

    Start adding marina maps to Storage pages

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,31 +1,3 @@
-// // import getBillboard from "@/actions/get-billboard";
-// // import getProducts from "@/actions/get-products";
-// // import ProductList from "@/components/product-list";
-// // import Billboard from "@/components/ui/billboard";
-// // import Container from "@/components/ui/container";
-
-// // export const revalidate = 0;
-
-// // const HomePage = async () => {
-// //   const products = await getProducts({ isFeatured: true });
-// //   const billboard = await getBillboard("d90af64f-6ae6-465b-a912-9d9fd0f34742");
-
-// //   return (
-// //     <Container>
-// //       <div className="space-y-10 pb-10">
-// //         <Billboard 
-// //           data={billboard}
-// //         />
-// //         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-// //           <ProductList title="Featured Products" items={products} />
-// //         </div>
-// //       </div>
-// //     </Container>
-// //   )
-// // };
-
-// // export default HomePage;
-
 import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
 import Billboard from "@/components/ui/billboard";
