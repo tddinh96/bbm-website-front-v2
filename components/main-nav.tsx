5a807cc51e4a6cb13a2464756a38b2a9cc9ee7bf
tddinh96 <78867116+tddinh96@users.noreply.github.com>
2023-08-24 12:22:48 -0700

    set main-nav class to h-auto to dynamically response to small screen size

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -23,7 +23,7 @@ const MainNav: React.FC<MainNavProps> = ({
 
   return (
     <nav
-      className="mx-6 flex items-center space-x-4 lg:space-x-6"
+      className="mx-6 flex items-center space-x-4 lg:space-x-6 sm:py-12"
     >
       {routes.map((route) => (
         <Link
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,13 +14,14 @@ const Navbar = async () => {
     <div className="border-b">
       <Container>
         {/* Use flex-col and sm:flex-row to break the elements into multiple row when the screen size is small */}
-        <div className="relative px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row h-16 items-center">
+        {/* Adjust h-auto to dynamically adjust the nav-bar height based on the number of cols required */}
+        <div className="relative px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row sm:col-auto h-auto items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             {/* <p className="font-bold text-xl">Home</p> */}
             <Logo />
           </Link>
           <MainNavStatic className="mx-6" />
-          <MainNav data={categories} />
+          <MainNav data={categories}/>
           {/* <NavbarActions /> */}
         </div>
       </Container>
