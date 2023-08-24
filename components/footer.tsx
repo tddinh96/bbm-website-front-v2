d1dccbd6f673b62c7c740ca9d1ef376256555190
tddinh96 <78867116+tddinh96@users.noreply.github.com>
2023-08-24 09:53:10 -0700

    Add phone and email to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,14 +3,19 @@ const Footer = () => {
   return (
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
-        <p className="text-center text-xs text-black">
-          &copy; 2023 Big Break Marina Holdings LLC. All rights reserved.
+        <div className="text-center text-xs text-black">
+          <p>
+            &copy; 2023 Big Break Marina Holdings LLC. All rights reserved.
+            <br />
+            100 Big Break Road, Oakley CA 94561
+          </p>
+          <a href="tel:+19256790900" className="underline">Phone: 925 679 0900</a>
           <br />
-          100 Big Break Road, Oakley CA 94561
-        </p>
+          <a href="mailto:info@bigbreakmarina.com" className="underline">Email: info@bigbreakmarina.com</a>
+        </div>
       </div>
     </footer>
-  )
+  );
 };
 
 export default Footer;
