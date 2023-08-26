4e02cafa66bdc858ab4ce37d703db9d9891b5079
tddinh96 <78867116+tddinh96@users.noreply.github.com>
2023-08-25 19:55:07 -0700

    Remove UserMenu

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -38,7 +38,7 @@ const UserMenu = () => {
       >
         <User size={20} color="black" />
       </Button> */}
-      <div
+      {/* <div
         onClick={toggleOpen}
         className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
       >
@@ -70,53 +70,10 @@ const UserMenu = () => {
             </div>
           </div>
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
 
 export default UserMenu;
 
-// "use client";
-// import {AiOutlineMenu} from "react-icons/ai";
-// import Avatar from "./avatar";
-
-// const UserMenu = () => {
-//   return (
-//     <div className="relative ml-auto">
-//       <div className="flex flex-row gap-3">
-//         <div
-//           onClick={() => {}}
-//           className=" text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
-//         >
-//           Look for A Space
-//         </div>
-//         <div
-//           onClick={() => {}}
-//           className="
-//           p-4
-//           md:py-1
-//           md:px-2 b
-//           border-[1px]
-//           border-neutral-200
-//           flex
-//           flex-row
-//           items-center
-//           gap-3
-//           rounded-full
-//           cursor-pointer
-//           hover:shadow-md
-//           transition
-//           "
-//         >
-//           <AiOutlineMenu />
-//           <div className="">
-//             <Avatar />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default UserMenu;
