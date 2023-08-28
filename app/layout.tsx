026db4f5a72e48e1b8e9e8cf483413fc1b650ee0
tddinh96 <78867116+tddinh96@users.noreply.github.com>
2023-08-28 11:49:48 -0700

    Add Hubspot chat functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
-import { Urbanist, Nunito } from 'next/font/google'
+import { Urbanist, Nunito } from "next/font/google";
 
-import ModalProvider from '@/providers/modal-provider'
-import ToastProvider from '@/providers/toast-provider'
-import Navbar from '@/components/navbar'
-import Footer from '@/components/footer'
-import { Analytics } from '@vercel/analytics/react';
+import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import { Analytics } from "@vercel/analytics/react";
 
-import './globals.css'
-import Modal from '@/components/modals/modal'
-import ClientOnly from '@/components/client-only'
-import RegisterModal from '@/components/modals/register-modal'
+import "./globals.css";
+import Modal from "@/components/modals/modal";
+import ClientOnly from "@/components/client-only";
+import RegisterModal from "@/components/modals/register-modal";
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Big Break Marina',
-  description: 'Nature inside City Center.',
-}
+  title: "Big Break Marina",
+  description: "Nature inside City Center.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -29,14 +29,23 @@ export default function RootLayout({
         <ToastProvider />
         <ModalProvider />
         <ClientOnly>
-        {/* <Modal title='Hello' actionLabel='Submit' isOpen/> */}
-        <RegisterModal />
+          {/* <Modal title='Hello' actionLabel='Submit' isOpen/> */}
+          <RegisterModal />
         </ClientOnly>
         <Navbar />
         {children}
         <Analytics />
         <Footer />
+        {/* <!-- Start of HubSpot Embed Code --> */}
+        <script
+          type="text/javascript"
+          id="hs-script-loader"
+          async
+          defer
+          src="//js-na1.hs-scripts.com/43689572.js"
+        ></script>
+        {/* <!-- End of HubSpot Embed Code --> */}
       </body>
     </html>
-  )
+  );
 }
